2dc3dd6ad8cae28e2dd7776ffc20a6288f4d36e1
Adam Goossens <adam.goossens@gmail.com>
2021-09-07 11:47:32 +1000

    TWilio integration with feature gate

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from twilio.rest import Client
 from aiokafka import AIOKafkaConsumer
 import asyncio, os, ast , sys
 import nest_asyncio
@@ -9,12 +10,19 @@ KAFKA_TOPIC = os.getenv('KAFKA_TOPIC', 'lpr')
 KAFKA_CONSUMER_GROUP_ID = os.getenv('KAFKA_CONSUMER_GROUP_ID', 'event_consumer_group')
 loop = asyncio.get_event_loop()
 
-## Database details and connection
-DB_USER = os.getenv('DB_USER', 'dbadmin')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'HT@1202k')
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
-DB_NAME = os.getenv('DB_NAME','pgdb')
-TABLE_NAME = os.getenv('TABLE_NAME','event')
+TWILIO_CLIENT_ID = os.getenv('TWILIO_CLIENT_ID')
+TWILIO_CLIENT_KEY = os.getenv('TWILIO_CLIENT_KEY')
+TWILIO_DESTINATION_NUMBER = os.getenv('TWILIO_DESTINATION_NUMBER')
+TWILIO_FROM_NUMBER = os.getenv('TWILIO_SOURCE_NUMBER')
+
+twilio_is_on = os.getenv('TWILIO_ENABLED', 'false') != 'false'
+
+watch_for_plates = ["2216E06", "S7JDV", "LCA2555"]
+
+anon_source_number = TWILIO_FROM_NUMBER[0:3] + "..." + TWILIO_FROM_NUMBER[-3:]
+anon_dest_number = TWILIO_DESTINATION_NUMBER[0:3] + "..." + TWILIO_DESTINATION_NUMBER[-3:]
+
+twilio = Client(TWILIO_CLIENT_ID, TWILIO_CLIENT_KEY)
 
 async def consume():
     kafkaConsumer = AIOKafkaConsumer(KAFKA_TOPIC, loop=loop, bootstrap_servers=KAFKA_ENDPOINT, group_id=KAFKA_CONSUMER_GROUP_ID)
@@ -26,10 +34,22 @@ async def consume():
             payload=ast.literal_eval(message.decode('utf-8'))
             plate = payload['event_vehicle_detected_plate_number']
             when = payload['event_timestamp']
+
+            sms_body = f"ALERT: Plate of interest {plate} detected at station {station} at {when}"
             try:
-                print("===============================================")
-                print(payload)
-                print("===============================================")
+                if plate in watch_for_plates:
+                    if twilio_is_on:
+                        client.messages.create(to=TWILIO_DESTINATION_NUMBER,
+                                               from_=TWILIO_FROM_NUMBER,
+                                               body=sms_body)
+                    print("===============================================")
+                    print(f"Detected plate of interest: {plate}")
+                    print(f"Is Twilio on? f{twilio_is_on}")
+                    print(f"Sent SMS to f{anon_dest_number} from f{anon_source_number}: 'f{sms_body}'")
+                    print("===============================================")
+                else:
+                    print("Plate {} not of interest.".format(plate))
+
             except Exception as e:
                 print(e)
                 print("Exiting ....")
